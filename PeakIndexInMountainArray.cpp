b755f42cc90fd0494cad0a31c42fef206110b079
NILOTPAL MISHRA <nilotpal.m083@gmail.com>
2026-03-08 06:51:45 +0000

    Implement solution for PeakIndexInMountainArray

---
+++ PeakIndexInMountainArray.cpp
@@ -0,0 +1,36 @@
+// You are given an integer mountain array arr of length n where the values increase to a peak element and then decrease.
+// Return the index of the peak element.
+// Your task is to solve it in O(log(n)) time complexity.
+// Constraints:
+// 3 <= arr.length <= 105
+// 0 <= arr[i] <= 106
+// arr is guaranteed to be a mountain array.
+
+ 
+// Example 1:
+// Input: arr = [0,1,0]
+// Output: 1
+
+// Example 2:
+// Input: arr = [0,2,1,0]
+// Output: 1
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int st=1,end=arr.size()-2;
+        while(st<=end){
+            int mid=st+(end-st)/2;
+            if(arr[mid-1]<arr[mid] && arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            else if(arr[mid-1]<arr[mid]){
+                st=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
+};
